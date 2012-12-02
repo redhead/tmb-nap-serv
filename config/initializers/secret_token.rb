c13f7bdbeccfd65cd20e5e645dbf5713980e7403
medy75 <jarin.md75@gmail.com>
2012-12-02 21:05:56 +0100

    New project with modified Gemlock file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TmbNapServ::Application.config.secret_token = '564e9b620d034cfefb887c5d6f012becd1d91160892724842a65656ab5f79a9a2af50e3608437f4db3110521a1e7793b4f3fac356d7336326cdc63ba3dd64cdf'
