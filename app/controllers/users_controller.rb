2b0d35c63f00facbcecabb7e92ae5af79b4db38a
medy75 <jarin.md75@gmail.com>
2012-12-03 09:20:13 +0100

    Added json support into user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
   	@users = User.all
+
+  	respond_to do |format|
+  		format.json { render :json => @users.to_json}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 TmbNapServ::Application.routes.draw do
   
+  root :to => "users#index"
+
   resources :users
 
   # The priority is based upon order of creation:
